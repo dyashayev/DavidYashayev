3ba1e2a81a60b0f9aff5f18230e4028ad0f77458
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2017-10-23 11:40:59 -0400

    cave Stuff

---
+++ David Yashayev/src/caveExplorer/CaveExplorer.java
@@ -0,0 +1,14 @@
+package caveExplorer;
+
+public class CaveExplorer {
+	
+	public static CaveRoom[][] caves;//every room in the cave
+	public static Scanner in;//for user input
+	public static CaveRoom currentRoom;//changes based on user's navigation
+	public static Inventory inventory;//where all objects found in cave are kept
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
